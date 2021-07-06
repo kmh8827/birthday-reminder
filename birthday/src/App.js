72fdc00464a01f1873b66e667420fc2463e58afd
kevin <kevinhellman94@gmail.com>
2021-07-06 15:21:33 -0400

    Initial Commit

---
+++ birthday/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
+const App = () => {
+  return <h2>reminder project setup</h2>;
+
+}
+
+export default App;
