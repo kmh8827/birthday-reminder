350733290df0a8a989281c2069f4531bbba2a7dd
kevin <kevinhellman94@gmail.com>
2021-07-06 15:30:47 -0400

    Displaying all information fro Data in List componenet

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -3,8 +3,19 @@ import data from './data';
 import List from './List';
 
 const App = () => {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
 
+  return (
+  <main>
+    <section className="container">
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
+      <button onClick={() => console.log('you clicked me!')}>
+        clear all
+      </button>
+    </section>
+  </main>
+  );
 }
 
 export default App;
